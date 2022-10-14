2a139de46ebbc0635b32c28210cd4fecc068fb48
SteveRidout <steveridout@gmail.com>
2022-10-14 15:37:50 +0200

    Remove /playground scripts

--- playground/alternativeBuffer.ts
+++
@@ -1,9 +0,0 @@
-import * as ansiEscapes from "../src/ansiEscapes";
-
-process.stdout.write(ansiEscapes.enableAlternativeBuffer);
-console.log("hello, this is an alternative buffer");
-
-setTimeout(() => {
-  process.stdout.write(ansiEscapes.disableAlternativeBuffer);
-  process.exit();
-}, 3000);
--- playground/readline.ts
+++
@@ -1,7 +0,0 @@
-import readline from "readline";
-
-const rl = readline.createInterface(process.stdin, process.stdout);
-
-rl.question("Type!", (answer) => {
-  console.log("Got answer: ", answer);
-});
