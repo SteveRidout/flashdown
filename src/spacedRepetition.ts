3f759a37cd02deb30e7bc0b916b24d58ab26f9c7
SteveRidout <steveridout@gmail.com>
2022-10-21 23:41:14 +0200

    Improve SRS algo

--- src/spacedRepetition.ts
+++ src/spacedRepetition.ts
@@ -1,5 +1,7 @@
 import { CardLearningDerivedMetrics, PracticeRecord } from "./types";
 
+import * as debug from "./debug";
+
 // The spaced repetition algorithm is inspired by SM2, originally used in SuperMemo and a variant of
 // which is used in the popular flashcard app Anki.
 
@@ -17,11 +19,15 @@ interface SpacedRepetitionConfig {
    * A score of 2 results in a time half of this and a score of 4 results in double this.
    */
   firstInterval: number;
+
+  /** The minimum time we can wait until showing the user a card again after they got it correct */
+  minimumCorrectInterval: number;
 }
 
 const defaultConfig: SpacedRepetitionConfig = {
   initialEasinessFactor: 2.5,
   firstInterval: 60 * 24,
+  minimumCorrectInterval: 60,
 };
 
 export const getSpacedRepetitionInfo = (
@@ -44,6 +50,16 @@ export const getSpacedRepetitionInfo = (
       throw Error("Practice records not in chronological order");
     }
 
+    const previousInterval =
+      previous === undefined ? undefined : record.practiceTime - previous.time;
+
+    // If previous interval is very short, indicating a practice within the timescale of a single
+    // session, don't do anything, the future scheduling will be based only on the user's
+    // performance on the first time they saw it within a session
+    if (previousInterval !== undefined && previousInterval < 5) {
+      continue;
+    }
+
     const easinessDelta = (() => {
       switch (record.score) {
         case 1:
@@ -61,34 +77,40 @@ export const getSpacedRepetitionInfo = (
 
     easinessFactor = Math.max(1.3, easinessFactor + easinessDelta);
 
-    const previousInterval =
-      previous === undefined ? undefined : record.practiceTime - previous.time;
+    debug.log("previous interval: " + previousInterval);
 
     const nextInterval = (() => {
       switch (record.score) {
         case 1:
-          // Schedule for 6 hours time
-          return 6 * 60;
+          // Schedule for 1/4 of the previous interval, or in 1 minute's time if there is no
+          // previous interval, allowing the user to re-practice this immediately after this
+          // session if they want to.
+          return previousInterval === undefined ? 1 : previousInterval / 4;
         case 2:
         case 3:
         case 4:
           if (previousInterval === undefined || previous === undefined) {
-            // Schedule for the next day
+            // Schedule the first interval multiplied by a factor based on the score
             return config.firstInterval * Math.pow(2, record.score - 3);
           }
 
-          const interval = previousInterval * easinessFactor;
+          let interval = previousInterval * easinessFactor;
 
           // Since the user got this right, let's ensure that the next interval can't decrease
           // compared to the previous one. This is only for the case where the user happened to
           // practice before the scheduled time, e.g. perhaps they are cramming before an exam.
-          return Math.max(previous.nextInterval, interval);
+          interval = Math.max(previous.nextInterval, interval);
+
+          // Enforce the minimum interval duration for correct answers
+          interval = Math.max(config.minimumCorrectInterval, interval);
+
+          return interval;
         default:
           throw Error("Invalid score");
       }
     })();
 
-    nextPracticeTime = record.practiceTime + nextInterval;
+    nextPracticeTime = record.practiceTime + nextInterval / 2;
 
     previous = {
       time: record.practiceTime,
