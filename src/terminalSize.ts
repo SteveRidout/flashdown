f6af3f194093ba5863fe9b4fac3cbb96b62ac31c
SteveRidout <steveridout@gmail.com>
2022-10-24 19:15:26 +0200

    1 column right hand margin

--- src/terminalSize.ts
+++ src/terminalSize.ts
@@ -1,4 +1,8 @@
 import * as config from "./config";
 
 export const getWidth = () =>
-  Math.min(config.get().maxColumnWidth, process.stdout.columns);
+  Math.min(
+    config.get().maxColumnWidth,
+    // Remove 1 to add a right hand margin
+    process.stdout.columns - 1
+  );
